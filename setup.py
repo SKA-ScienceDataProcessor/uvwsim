87e6848e9a0573a04c1879b16a7cb3b9398d6fcf
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2016-02-09 12:57:52 +0000

    minor update to python module build script

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from os.path import join
+from numpy import get_include
 try:
     from setuptools import setup, Extension
 except ImportError:
     from distutils.core import setup, Extension
 
-from os import path
-from os.path import join
-
-from numpy import get_include
-numpy_inc = get_include()
 
 def get_version():
+    """Return the version of pyuvwsim"""
     context = {}
-    version_file = join('pyuvwsim','version.py')
+    version_file = join('pyuvwsim', 'version.py')
     try:
         execfile
     except NameError:
@@ -22,20 +21,22 @@ def get_version():
         execfile(version_file, context)
     return context['__version__']
 
-_pyuvwsim = Extension('pyuvwsim._pyuvwsim',
-        sources=[join('pyuvwsim','src','pyuvwsim.c'), join('src','uvwsim.c')],
-        include_dirs=[numpy_inc, 'src', join('pyuvwsim','src')],
-        language='c')
 
-extension_list = [_pyuvwsim]
+numpy_inc = get_include()
+pyuvwsim_extension = Extension('pyuvwsim._pyuvwsim',
+                               sources=[join('pyuvwsim', 'src', 'pyuvwsim.c'),
+                                        join('src', 'uvwsim.c')],
+                               include_dirs=[numpy_inc, 'src',
+                                             join('pyuvwsim', 'src')],
+                               language='c')
 
 setup(
     name='pyuvwsim',
     version=get_version(),
     description="A simple python API for generating interferometric baseline "
-        "(uvw) coordinates",
+                "(uvw) coordinates",
     packages=['pyuvwsim'],
-    ext_modules=extension_list,
+    ext_modules=[pyuvwsim_extension],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -44,15 +45,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX',
         'Programming Language :: C',
-        'Programming Language :: C++',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
-    author='',
-    author_email='',
+    author='Oxford University e-Research Centre',
+    author_email='benjamin.mort@oerc.ox.ac.uk',
     url='https://github.com/SKA-ScienceDataProcessor/uvwsim',
     license='BSD',
     install_requires=['numpy']
